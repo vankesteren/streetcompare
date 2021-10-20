c0f57427f808475a5e4ddd8caf0421255243291d
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2021-10-20 09:12:56 +0200

    bomen + cleanup voorzieningen (#3)
    also buurten plot

--- 03_stedelijkebuurtenvoorzieningen.R
+++ 03_stedelijkebuurtenvoorzieningen.R
@@ -1,7 +1,9 @@
+# Generate distribution plots for voorzieningen per buurt
 library(tidyverse)
 library(sf)
 library(cbsodataR)
 
+# data prep ----
 # voorzieningen totaal
 voorzieningen_nabijheid <- cbs_get_data("84463NED")
 
@@ -27,32 +29,71 @@ buurt_df_long <-
     names_sep = "_"
   )
 
-
-
 einst_df_long <- 
   buurt_df_long %>% 
   filter(BU_CODE %in% c("BU03440321",  "BU03440322", "BU03440331", "BU03440341")) %>% 
   mutate(BU_NAAM = str_remove(BU_NAAM, "\\ .+"))
 
-
-
+# plotting ----
 
 # nu kunnen we plotten 
-vz_list <- c("Bioscoop", "Museum", "PodiumkunstenTotaal", 
-             "Restaurant", "Warenhuis", "Ziekenhuis", 
-             "Apotheek", "Bibliotheek", "Kinderdagverblijf")
+bdf <- buurt_df_long %>% filter(STED == 1, ColNum != 11) 
+edf <- einst_df_long %>% filter(STED == 1, ColNum != 11) 
 
-bdf <- buurt_df_long %>% filter(STED == 1, Voorziening %in% vz_list, ColNum != 11) 
-edf <- einst_df_long %>% filter(STED == 1, Voorziening %in% vz_list, ColNum != 11) 
+# entertainment
+entertainment <- c("Bioscoop", "Museum", "PodiumkunstenTotaal", "Zwembad", "Restaurant") 
 
 bdf %>% 
+  filter(Voorziening %in% entertainment) %>% 
   ggplot(aes(x = Afstand)) +
   geom_histogram() +
-  geom_vline(data = edf, aes(xintercept = Afstand, colour = BU_NAAM)) +
+  geom_vline(data = edf %>% filter(Voorziening %in% entertainment), 
+             aes(xintercept = Afstand, colour = BU_NAAM)) +
   facet_wrap(~Voorziening, scales = "free") +
-  labs(x = "Afstand (km)", y = "Aantal stedelijke buurten", colour = "Buurt") +
+  labs(x = "Afstand (km)", y = "Aantal stedelijke buurten", colour = "Buurt", 
+       title = "Voorzieningenprofiel Einsteindreef") +
   theme_minimal() +
   scale_colour_viridis_d() +
   theme(legend.position = "top")
 
-ggsave("fig/einstein_profiel.png", width = 9, height = 6)
+ggsave("fig/dist/einstein_profiel_entertainment.png", width = 12, height = 7, bg = "white")
+
+# zorg / veiligheid
+zorg <- c("Ziekenhuis", "Brandweerkazerne", "Huisartsenpost", "Kinderdagverblijf")
+
+bdf %>% 
+  filter(Voorziening %in% zorg) %>% 
+  ggplot(aes(x = Afstand)) +
+  geom_histogram() +
+  geom_vline(data = edf %>% filter(Voorziening %in% zorg), 
+             aes(xintercept = Afstand, colour = BU_NAAM)) +
+  facet_wrap(~Voorziening, scales = "free") +
+  labs(x = "Afstand (km)", y = "Aantal stedelijke buurten", colour = "Buurt", 
+       title = "Voorzieningenprofiel Einsteindreef") +
+  theme_minimal() +
+  scale_colour_viridis_d() +
+  theme(legend.position = "top")
+
+ggsave("fig/dist/einstein_profiel_zorg.png", width = 12, height = 7, bg = "white")
+
+
+
+# overig
+overig <- c("Restaurant", "Bibliotheek", "Volkstuin")
+
+
+bdf %>% 
+  filter(Voorziening %in% overig) %>% 
+  ggplot(aes(x = Afstand)) +
+  geom_histogram() +
+  geom_vline(data = edf %>% filter(Voorziening %in% overig), 
+             aes(xintercept = Afstand, colour = BU_NAAM)) +
+  facet_wrap(~Voorziening, scales = "free") +
+  labs(x = "Afstand (km)", y = "Aantal stedelijke buurten", colour = "Buurt", 
+       title = "Voorzieningenprofiel Einsteindreef") +
+  theme_minimal() +
+  scale_colour_viridis_d() +
+  theme(legend.position = "top")
+
+ggsave("fig/dist/einstein_profiel_overig.png", width = 12, height = 7, bg = "white")
+
